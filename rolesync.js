4a5d77888b8fea7ba916a56fb6209ac0c1b6926c
Junior Conte <juniorcnt@gmail.com>
2018-01-27 01:19:38 -0200

    ADD: Command to init new config file

--- rolesync.js
+++ rolesync.js
@@ -3,6 +3,13 @@
 
 var program = require('commander');
 var pkg = require('./package.json');
+var init = require('./lib/rolesync-init')
 
 program.version(pkg.version);
+
+program
+  .command('init')
+    .description('Create new rolesync config file')
+    .action(init);
+
 program.parse(process.argv);
